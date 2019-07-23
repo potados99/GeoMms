7b08ddc044b452bec93103b0bbadce8e61c24efe
potados99 <potados99@gmail.com>
2019-07-23 17:57:51 +0900

    Done: NavigationBasedFragment.

--- app/src/main/java/com/potados/geomms/core/platform/NavigationBasedFragment.kt
+++ app/src/main/java/com/potados/geomms/core/platform/NavigationBasedFragment.kt
@@ -1,5 +1,12 @@
 package com.potados.geomms.core.platform
 
+/**
+ * Base class for Fragment which is a member of [NavigationBasedActivity].
+ */
 abstract class NavigationBasedFragment : BaseFragment() {
+
+    /**
+     * Id of menu item where this Fragment is mapped.
+     */
     abstract fun menuItemId(): Int
 }
