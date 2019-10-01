110acf6aae0d93aa18b668b27d53ff1e8ead707c
potados99 <potados99@gmail.com>
2019-10-01 13:03:44 +0900

    Fix korean search.

--- common/src/main/java/com/potados/geomms/extension/String.kt
+++ common/src/main/java/com/potados/geomms/extension/String.kt
@@ -48,4 +48,4 @@ import java.text.Normalizer
 /**
  * Strip the accents from a string
  */
-fun CharSequence.removeAccents() = Normalizer.normalize(this, Normalizer.Form.NFD)
+fun CharSequence.removeAccents() = Normalizer.normalize(this, Normalizer.Form.NFD)!!
--- data/src/main/java/com/potados/geomms/filter/ContactFilter.kt
+++ data/src/main/java/com/potados/geomms/filter/ContactFilter.kt
@@ -21,11 +21,14 @@ package com.potados.geomms.filter
 
 import com.potados.geomms.extension.removeAccents
 import com.potados.geomms.model.Contact
+import timber.log.Timber
 
 class ContactFilter(private val phoneNumberFilter: PhoneNumberFilter) : Filter<Contact>() {
 
     override fun filter(item: Contact, query: CharSequence): Boolean {
-        return item.name.removeAccents().contains(query, true) || // Name
+        // Remember to apply removeAccents to the query TOO.
+        // Or the query will not match the normalized unicode string.
+        return item.name.removeAccents().contains(query.removeAccents(), true) || // Name
                 item.numbers.map { it.address }.any { address -> phoneNumberFilter.filter(address, query) } // Number
     }
 
